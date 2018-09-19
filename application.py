fb9a080dc02c5c5bb0048753e34a11b71dcf6dca
Quinn Batten <quinnbatten@gmail.com>
2018-09-19 16:08:04 -0400

    added ability to add new band names to json file while maintaining order of addition.

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect
 import json
 from random import randint, shuffle
+from re import sub
 
 # Configure app
 app = Flask(__name__)
@@ -25,23 +26,40 @@ def index():
     """Render main page"""
     if request.method == "GET":
         # Get band names
-        shuffle(band_names)
+        band_names_tmp = band_names[:]
+        shuffle(band_names_tmp)
         # Generate CSS
         gen_css = []
-        for i, item in enumerate(band_names):
+        for i, item in enumerate(band_names_tmp):
             # Randomization
             rand_1 = randint(5, 600)
             rand_2 = randint(0, 40)
             r = lambda: randint(0,255)
             color = format('#%02X%02X%02X' % (r(),r(),r()))
-            fontrand = lambda: randint(0, len(fonts))
+            fontrand = randint(0, len(fonts) - 1)
             font = fonts[fontrand]
             # Create individual CSS string
             tmp = "#name-" + str(i) + "{width:10%; padding-left: " + str(rand_1) + "px; padding-top: " + str(rand_2) + "px; color: " + color + "; font-family: " + font + ";}"
             gen_css.append(tmp)
         return render_template("index.html",
-                               band_names=band_names,
+                               band_names=band_names_tmp,
                                gen_css=gen_css)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/submit", methods=["GET", "POST"])
+def submit():
+    if request.method == "GET":
+        return render_template("submit.html")
+
     if request.method == "POST":
-        band_names.append(request.form["contents"])
-        return redirect(url_for('index'))
+        submission = request.form.get("bandname")
+        submission = sub(r'[^\w\. ]*', '', submission)
+        band_names.append(submission)
+        with open('static/bandnames.json', 'w') as f:
+            json.dump(band_names, f)
+        return redirect('/')
